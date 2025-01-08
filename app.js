f383aa37a8901f05df57110008764bc232f95ecd
vivek122023 <153589831+vivek122023@users.noreply.github.com>
2025-01-08 14:19:08 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,76 @@
+let boxes = document.querySelectorAll(".box");
+
+let resetBtn = document.querySelector("#reset-btn");
+
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector(".msg");
+let turn0 = true;//playerX, player0
+
+const winPattern = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+
+];
+const resetGame = () => {
+    turn0 = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+};
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if (turn0) {//playerO
+            box.innerText = "0";
+            turn0 = false;
+        } else {//playerX
+            box.innerText = "X";
+            turn0 = true;
+        }
+        box.disabled = true;
+        checkWinner();
+    });
+});
+
+const disabledBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = true;
+    };
+}
+
+const enableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = false;
+        box.innerText = "";
+    }
+};
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disabledBoxes();
+
+};
+
+const checkWinner = () => {
+    for (let pattern of winPattern) {
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+        if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+            if (pos1Val === pos2Val && pos2Val === pos3Val) {
+                showWinner(pos1Val);
+            }
+        }
+    }
+};
+resetBtn.addEventListener("click", resetGame);
+newGameBtn.addEventListener("click", resetGame);
+
+
